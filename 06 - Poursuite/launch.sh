8f4a2d04717531da3519402981822e2a0fa7d5c2
Jean-Michel Paris <jmparis@hotmail.fr>
2025-11-13 00:06:32 +0100

    feat(script): Launch app within venv

---
+++ 06 - Poursuite/launch.sh
@@ -0,0 +1,10 @@
+# Launch script for 01 - L'Échiquier inversé
+
+# Activate the local virtual environment
+source ../.venv/bin/activate
+
+# Run the example
+python poursuite.py
+
+# Deactivate the virtual environment
+deactivate
